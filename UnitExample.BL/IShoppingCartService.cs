eb913db75a6fe3971c767a00be720c95d1b0da3d
Caner Al <caneral5858@gmail.com>
2022-01-23 17:40:12 +0300

    * IShoppingCartService.cs: Service added
    * UnitExample.BL.csproj:
    * ShoppingCartService.cs:
    * ShoppingCartServiceFake.cs:

---
+++ UnitExample.BL/IShoppingCartService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using UnitExample.Entity;
+
+namespace UnitExample.BL
+{
+    public interface IShoppingCartService
+    {
+        IEnumerable<ShoppingItem> GetAllItems();
+        ShoppingItem Add(ShoppingItem newItem);
+        ShoppingItem GetById(Guid id);
+        void Remove(Guid id);
+    }
+}
---
+++ UnitExample.BL/ShoppingCartService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnitExample.Entity;
+
+namespace UnitExample.BL
+{
+    public class ShoppingCartService : IShoppingCartService
+    {
+        
+            public ShoppingItem Add(ShoppingItem newItem) => throw new NotImplementedException();
+            public IEnumerable<ShoppingItem> GetAllItems() => throw new NotImplementedException();
+            public ShoppingItem GetById(Guid id) => throw new NotImplementedException();
+            public void Remove(Guid id) => throw new NotImplementedException();
+    }
+}
